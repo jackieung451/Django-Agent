dae7d15c036595188d63be5be6e00db54734063c
jackieung451 <jackieung451@gmail.com>
2021-09-22 12:58:43 -0700

    Updated Views Logic and Urls for Login Users and JWT Cookie Session Authorization and Authentication

--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+from core.models import User
+
+
+class AgentAPIView(APIView):
+    def get(self, _):
+        agents = User.objects.filter(is_agent=True)
--- common/views.py
+++ common/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import exceptions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.models import User
+from .authenitication import JWTAuthentication
 from .serializers import UserSerializer
 
 
@@ -14,7 +16,7 @@ class RegisterAPIView(APIView):
         if data['password'] != data['password_confirm']:
             raise exceptions.APIException('Passwords do not match!')
 
-        data['is_agent'] = 'api/agent' in request.path
+        data['is_agent'] = 0
 
         serializer = UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -34,4 +36,56 @@ class LoginAPIView(APIView):
         if not user.check_password(password):
             raise exceptions.AuthenticationFailed('Incorrect Password!')
 
+        token = JWTAuthentication.generate_jwt(user.id)
+
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'message': 'success'
+        }
+        return response
+
+class UserAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
+
+class LogoutAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, _):
+        response = Response()
+        response.delete_cookie(key='jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
+
+class ProfileInfoAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk=None):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class ProfilePasswordAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk=None):
+        user = request.user
+        data = request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Passwords do not match!')
+
+        user.set_password(data['password'])
+        user.save()
         return Response(UserSerializer(user).data)
