df4af643b4d878d671a90e9a51e9f392748768c3
jackieung451 <jackieung451@gmail.com>
2021-09-22 02:49:24 -0700

    First Commit

--- common/views.py
+++ common/views.py
@@ -3,6 +3,7 @@ from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from core.models import User
 from .serializers import UserSerializer
 
 
@@ -19,3 +20,18 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Incorrect Password!')
+
+        return Response(UserSerializer(user).data)
